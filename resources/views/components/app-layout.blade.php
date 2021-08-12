ebaadbb30c4a0730500f97ad3e98aac861d19748
Owen Conti <owen@ohseemedia.com>
2021-08-12 07:56:51 -0600

    Fix favicon path

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -15,8 +15,8 @@
         <!-- Fonts -->
         <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://rsms.me/inter/inter.css">
 
-        <link rel="icon" href="{{ asset('/favicon.svg') }}" sizes="any" type="image/svg+xml" />
-        <link rel="icon" href="{{ asset('/favicon.png') }}" type="image/png" />
+        <link rel="icon" href="{{ asset('/dist/favicon.svg') }}" sizes="any" type="image/svg+xml" />
+        <link rel="icon" href="{{ asset('/dist/favicon.png') }}" type="image/png" />
 
         <!-- Scripts -->
         @vite(js/app.js)
