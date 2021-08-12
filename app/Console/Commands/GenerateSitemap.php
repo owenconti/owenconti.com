33ecffd9c77f637e0d1d6a3db1a75f5df62de257
Owen Conti <owen@ohseemedia.com>
2021-08-11 22:29:17 -0600

    Generate sitemap command

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use App\Models\Page;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $sitemap = Sitemap::create()
+            ->add(
+                Url::create('/')
+                    ->setLastModificationDate(now())
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                    ->setPriority(1.0)
+            )
+            ->add(
+                Url::create('/analytics')
+                    ->setLastModificationDate(now())
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+            )
+            ->add(
+                Url::create('/articles')
+                    ->setLastModificationDate(now())
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                    ->setPriority(0.9)
+            );
+
+        Page::all()->each(function (Page $page) use ($sitemap) {
+            $sitemap->add(
+                Url::create($page->url)
+                    ->setLastModificationDate($page->updated_at)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+            );
+        });
+
+        Category::all()->each(function (Category $category) use ($sitemap) {
+            $sitemap->add(
+                Url::create($category->url)
+                    ->setLastModificationDate($category->pages()->orderBy('updated_at', 'desc')->first()->updated_at)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+            );
+        });
+
+        $sitemap->writeToFile(base_path('public/sitemap.xml'));
+    }
+}
