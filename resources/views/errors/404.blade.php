ce5901bcb22bf98eceacce801f8ba7858b54fd2c
Owen Conti <owen@ohseemedia.com>
2021-08-17 07:43:51 -0600

    Add a custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div>
+        <h1 class="text-4xl font-bold">Page not found!</h1>
+
+        <div class="mt-8">
+          <p>We're sorry, but the page you were trying to view does not exist.</p>
+          <p class="mt-2">Try searching for what you're looking for:</p>
+          <x-search class="mt-2" />
+        </div>
+    </div>
+</x-app-layout>
