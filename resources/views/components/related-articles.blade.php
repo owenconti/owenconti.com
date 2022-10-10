00fe843ac0c2b6c8e39c640e03af51cf0f40831f
Owen Conti <owen@ohseemedia.com>
2022-10-09 21:20:52 -0600

    fix: Reduce size of related articles in the sidebar

--- resources/views/components/related-articles.blade.php
+++ resources/views/components/related-articles.blade.php
@@ -1,5 +1,15 @@
 @if($relatedArticles->count())
-  <div {{ $attributes->merge(['class' => 'mt-8']) }}>
-    <x-article-list :articles="$relatedArticles" />
+<div class="mt-8">
+  <h3 class="text-xl font-bold">Related Articles</h3>
+  <div class="space-y-4 mt-2">
+    @foreach($relatedArticles as $article)
+    <h3>
+      <a href="{{ $article->url }}"
+        class="text-base font-medium hover:underline text-brand-dark hover:text-brand-dark-darken dark:text-brand-light dark:hover:text-brand-light-lighten">
+        {{ $article->title}}
+      </a>
+    </h3>
+    @endforeach
   </div>
+</div>
 @endif
