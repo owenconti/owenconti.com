dd0d544c4a081e6e75243502511e6e5c6f704940
Owen Conti <owen@springloaded.co>
2024-01-01 23:13:15 -0700

    Run phpcs fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -32,7 +33,7 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (\Throwable $e) {
+        $this->reportable(function (Throwable $e) {
         });
     }
 }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
+use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,7 @@ class RedirectIfAuthenticated
      *
      * @param string|null ...$guards
      */
-    public function handle(Request $request, \Closure $next, ...$guards)
+    public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
 
