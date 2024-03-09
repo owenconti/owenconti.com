ef0da921c885a72087d5b2f10ce160b90d2684fe
Owen Conti <owen@springloaded.co>
2024-03-09 16:36:15 -0700

    feat: Add link to torchlight.dev in the footer of the site.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,12 @@
 <footer class="max-w-6xl w-full mx-auto p-6 mt-12 border-t border-zinc-200 flex items-center justify-between dark:border-zinc-800">
-    <p class="text-sm text-center text-zinc-700 dark:text-zinc-500">
-        &copy; Owen Conti. All rights reserved.
-    </p>
+    <div>
+        <p class="text-sm text-zinc-700 dark:text-zinc-500">
+            &copy; Owen Conti. All rights reserved.
+        </p>
+        <p class="text-xs text-zinc-500 dark:text-zinc-700 mt-1">
+            Syntax highlighting provided by <a href="https://torchlight.dev" class="font-bold hover:underline">Torchlight.dev</a>.
+        </p>
+    </div>
 
     <div class="flex justify-center gap-6">
         @foreach($nav['links'] as $link)
