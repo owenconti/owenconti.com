7789422196f574ff753a51dfca14fd4d346851cd
Owen Conti <owen@springloaded.co>
2024-03-11 16:05:43 -0600

    fix: Adjust styles of two column layout

--- resources/views/components/two-column-layout.blade.php
+++ resources/views/components/two-column-layout.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <div class="flex">
-        <div class="hidden w-64 lg:w-full lg:max-w-xs lg:block">
+    <div class="flex justify-between gap-8">
+        <div class="hidden w-48 lg:w-64 md:block flex-shrink-0">
             <x-sidebar />
         </div>
 
-        <div class="flex-1 w-0 max-w-full lg:pl-20">
+        <div class="flex-1 max-w-2xl w-full mx-auto min-w-0">
             {{ $slot }}
         </div>
     </div>
