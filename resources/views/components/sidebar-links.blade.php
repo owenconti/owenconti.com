0bb4d1f884aaefd71e54ea72db28c600a36d9802
Owen Conti <owen@springloaded.co>
2024-03-11 16:21:18 -0600

    fix: Fix display of sidebar links after sidebar width changed

--- resources/views/components/sidebar-links.blade.php
+++ resources/views/components/sidebar-links.blade.php
@@ -1,6 +1,6 @@
-<div {{ $attributes->merge(['class' => 'grid grid-cols-2 gap-4']) }}>
+<div {{ $attributes->merge(['class' => 'flex flex-col lg:flex-row flex-wrap gap-2']) }}>
     @foreach($links as $link)
-        <a href="{{ $link['url'] }}" class="inline-flex items-center gap-4 py-2 px-4 text-sm font-bold text-white rounded-lg rounded-tl-none rounded-br-none bg-zinc-800 hover:bg-zinc-700 dark:text-zinc-800 dark:bg-zinc-50 hover:underline dark:hover:bg-zinc-200">
+        <a href="{{ $link['url'] }}" class="w-full flex-1 lg:flex-grow-0 flex lg:inline-flex items-center gap-2 py-2 px-3 text-sm font-bold text-white rounded-lg rounded-tl-none rounded-br-none bg-zinc-800 hover:bg-zinc-700 dark:text-zinc-800 dark:bg-zinc-50 hover:underline dark:hover:bg-zinc-200">
             <x-dynamic-component :component="$link['component']" class="w-4 h-4" />
             {{ $link['label'] }}
         </a>
