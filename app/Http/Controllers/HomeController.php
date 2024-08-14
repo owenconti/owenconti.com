974e2054b09b686fd4e0ee38ae2996f9995ea85e
Owen Conti <owen@springloaded.co>
2024-08-14 17:37:19 -0600

    refactor: Clean up Springloaded card

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
         return view('pages.home', [
             'articles' => Page::where('type', 'post')->limit(5)->latest()->get(),
             'cards' => [
-                ['label' => 'Springloaded', 'description' => 'Springloaded is the software development studio I co-founded with Jeff Sagal in April 2022.', 'icon' => 'springloaded-icon', 'url' => 'https://springloaded.co'],
+                ['label' => 'Springloaded', 'description' => 'Springloaded is the software development studio I co-founded and run.', 'icon' => 'springloaded-icon', 'url' => 'https://springloaded.co'],
                 ['label' => 'YouTube', 'description' => 'I post video versions of my articles on my YouTube channel.', 'icon' => 'youtube-icon', 'url' => 'https://youtube.com/owenconti'],
                 ['label' => 'GitHub', 'description' => 'I keep all of my development work and open source code on GitHub.', 'icon' => 'github-icon', 'url' => 'https://github.com/owenconti'],
                 ['label' => 'Chat with me on X', 'description' => 'My short form and conversational content is posted on X.', 'icon' => 'twitter-icon', 'url' => 'https://x.com/owenconti'],
