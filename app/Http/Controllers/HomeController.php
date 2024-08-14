1863b2cecd66b4bc553a1ecec7ac3cd3c6d3df2c
Owen Conti <owen@springloaded.co>
2024-08-14 17:36:51 -0600

    fix: Grammatical error

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
             'articles' => Page::where('type', 'post')->limit(5)->latest()->get(),
             'cards' => [
                 ['label' => 'Springloaded', 'description' => 'Springloaded is the software development studio I co-founded with Jeff Sagal in April 2022.', 'icon' => 'springloaded-icon', 'url' => 'https://springloaded.co'],
-                ['label' => 'YouTube', 'description' => 'I posted video versions of my articles on my YouTube channel.', 'icon' => 'youtube-icon', 'url' => 'https://youtube.com/owenconti'],
+                ['label' => 'YouTube', 'description' => 'I post video versions of my articles on my YouTube channel.', 'icon' => 'youtube-icon', 'url' => 'https://youtube.com/owenconti'],
                 ['label' => 'GitHub', 'description' => 'I keep all of my development work and open source code on GitHub.', 'icon' => 'github-icon', 'url' => 'https://github.com/owenconti'],
                 ['label' => 'Chat with me on X', 'description' => 'My short form and conversational content is posted on X.', 'icon' => 'twitter-icon', 'url' => 'https://x.com/owenconti'],
                 ['label' => 'Twitch', 'description' => 'I sometimes stream my side project and open source work on Twitch.', 'icon' => 'twitch-icon', 'url' => 'https://twitch.tv/owenconti'],
